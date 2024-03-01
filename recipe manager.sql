311ecdea64a70bb481372b01aeeb35cc0d6aec7a
SaraAlqallaf <157518700+SaraAlqallaf@users.noreply.github.com>
2024-03-01 14:21:29 -0500

    Add files via upload
    recipe manager.sql Updated to have procedures. 
    RecipeManagerGUI.java also updated with additional menu options

--- recipe manager.sql
+++ recipe manager.sql
@@ -5,3 +5,21 @@ userId integer not null unique auto_increment,
 userName varchar(255) not null,
 password varchar(255) not null
 );
+
+DELIMITER //
+CREATE PROCEDURE addnewUser (IN username VARCHAR(255), IN password VARCHAR(255))
+BEGIN
+    INSERT INTO users (userName, password) VALUES (username, password);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE loginCreds (IN username VARCHAR(255), IN password VARCHAR(255))
+BEGIN
+    SELECT * FROM users WHERE userName = username AND password = password;
+END //
+DELIMITER ;
+
+
+
+
