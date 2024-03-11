22f34e3443a052e358401e03f1e2406997dffda7
kirrafisher23 <103162191+kirrafisher23@users.noreply.github.com>
2024-03-11 16:08:50 -0400

    Update recipe manager.sql
    with the damn tables this time...

--- recipe manager.sql
+++ recipe manager.sql
@@ -1,25 +1,48 @@
-create schema recipeManager;
-
 create table users(
-userId integer not null unique auto_increment,
+userID integer not null auto_increment, 
 userName varchar(255) not null,
-password varchar(255) not null
+pass varchar(255) not null,
+primary key(userID)
 );
 
-DELIMITER //
+
+create table recipes(
+recipeId integer not null auto_increment,
+recipeName varchar(255) unique,
+creationDate date not null,
+modifyDate date not null,
+userID integer not null,
+recipeType varchar(255) not null,
+primary key(recipeId),
+FOREIGN KEY (userID) REFERENCES users(userID)
+);
+
+ create table ingredients(
+ingredientID integer not null auto_increment,
+ingredientType varchar(255) not null unique,
+ingredientName varchar(255) not null,
+primary key (ingredientID)
+ )
+ 
+ create table recipesIngredients(
+ recipeId integer not null,
+ ingredientID integer not null,
+ quantity  integer not null,
+ unit varchar(255) not null,
+ FOREIGN KEY (recipeId) REFERENCES recipes(recipeId),
+ FOREIGN KEY (ingredientID) REFERENCES ingredients(ingredientID)
+ );
+  DROP TABLE recipesIngredients;
+ 
+DELIMITER $$
 CREATE PROCEDURE addnewUser (IN username VARCHAR(255), IN password VARCHAR(255))
 BEGIN
     INSERT INTO users (userName, password) VALUES (username, password);
-END //
-DELIMITER ;
+END $$
 
-DELIMITER //
+
+DELIMITER $$
 CREATE PROCEDURE loginCreds (IN username VARCHAR(255), IN password VARCHAR(255))
 BEGIN
     SELECT * FROM users WHERE userName = username AND password = password;
-END //
-DELIMITER ;
-
-
-
-
+END$$ 
