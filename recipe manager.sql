0c5a851667d798f620947d4e065d49e1d4bbb000
kirrafisher23 <103162191+kirrafisher23@users.noreply.github.com>
2024-03-24 11:17:55 -0400

    New SQL with SP for adding recipe

--- recipe manager.sql
+++ recipe manager.sql
@@ -1,48 +1,91 @@
-create table users(
-userID integer not null auto_increment, 
-userName varchar(255) not null,
-pass varchar(255) not null,
-primary key(userID)
+CREATE TABLE `recipe` (
+  `recipe_id` int NOT NULL AUTO_INCREMENT,
+  `recipe_name` varchar(45) NOT NULL,
+  `recipe_time`time not NULL,
+  `recipe_serving_size` int DEFAULT NULL,
+  PRIMARY KEY (`recipe_id`),
+  UNIQUE KEY `recipe_name_UNIQUE` (`recipe_name`)
+);
+drop table recipe;
+
+CREATE TABLE `recipe_ingredients` (
+  `recipeID` int DEFAULT NULL,
+  `ingredientsID` int DEFAULT NULL,
+  `quantityID` int DEFAULT NULL,
+  `measurementsID` int DEFAULT NULL,
+  KEY `recipe_id_idx` (`recipeID`),
+  KEY `ingredientsID_idx` (`ingredientsID`),
+  KEY `quantityID_idx` (`quantityID`),
+  KEY `measurementsID_idx` (`measurementsID`),
+  CONSTRAINT `ingredientsID` FOREIGN KEY (`ingredientsID`) REFERENCES `ingredients` (`ingredient_id`),
+  CONSTRAINT `measurementsID` FOREIGN KEY (`measurementsID`) REFERENCES `measurements` (`measurement_id`),
+  CONSTRAINT `quantityID` FOREIGN KEY (`quantityID`) REFERENCES `quantity` (`quantity_id`),
+  CONSTRAINT `recipeID_ingredient_table` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`recipe_id`)
+);
+CREATE TABLE `recipe_steps` (
+  `steps_id` int NOT NULL AUTO_INCREMENT,
+  `steps` text,
+  `recipeID` int DEFAULT NULL,
+  PRIMARY KEY (`steps_id`),
+  KEY `recipe_id_idx` (`recipeID`),
+  CONSTRAINT `recipeID_steps` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`recipe_id`)
+);
+CREATE TABLE `recipe_tags` (
+  `recipeID` int DEFAULT NULL,
+  `categoryID` int DEFAULT NULL,
+  `tagID` int DEFAULT NULL,
+  KEY `recipeID_tags_idx` (`recipeID`),
+  KEY `categoryID_idx` (`categoryID`),
+  KEY `tagID_idx` (`tagID`),
+  CONSTRAINT `categoryID` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`category_id`),
+  CONSTRAINT `recipeID_tags` FOREIGN KEY (`recipeID`) REFERENCES `recipe` (`recipe_id`),
+  CONSTRAINT `tagID` FOREIGN KEY (`tagID`) REFERENCES `tags` (`tag_id`)
+);
+CREATE TABLE `tags` (
+  `tag_id` int NOT NULL AUTO_INCREMENT,
+  `tag` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`tag_id`),
+  UNIQUE KEY `tag_UNIQUE` (`tag`)
+);
+CREATE TABLE `categories` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `category_UNIQUE` (`category`)
+);
+CREATE TABLE `ingredients` (
+  `ingredient_id` int NOT NULL AUTO_INCREMENT,
+  `ingredient_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ingredient_id`)
+);
+CREATE TABLE `measurements` (
+  `measurement_id` int NOT NULL AUTO_INCREMENT,
+  `measurement` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`measurement_id`)
+);
+CREATE TABLE `quantity` (
+  `quantity_id` int NOT NULL AUTO_INCREMENT,
+  `quantity` float DEFAULT NULL,
+  PRIMARY KEY (`quantity_id`)
 );
 
+delimiter $$
+create procedure add_recipe(
+in recipeName varchar(255),
+in recipeTime time,
+in serveSize int,
+in ingredName text,
+in tag_call varchar(255),
+in step text
+)
+begin 
+insert into recipe(recipe_name,recipe_time,recipe_serving_size) 
+values (recipeName,recipeTime,serveSize);
+insert into ingredients(ingredient_name) 
+values(ingredName);
+insert into tags(tag) values (tag_call);
+insert into recipe_steps(steps) values (step);
+ end $$ 
 
-create table recipes(
-recipeId integer not null auto_increment,
-recipeName varchar(255) unique,
-creationDate date not null,
-modifyDate date not null,
-userID integer not null,
-recipeType varchar(255) not null,
-primary key(recipeId),
-FOREIGN KEY (userID) REFERENCES users(userID)
-);
+drop procedure add_recipe;
 
- create table ingredients(
-ingredientID integer not null auto_increment,
-ingredientType varchar(255) not null unique,
-ingredientName varchar(255) not null,
-primary key (ingredientID)
- )
- 
- create table recipesIngredients(
- recipeId integer not null,
- ingredientID integer not null,
- quantity  integer not null,
- unit varchar(255) not null,
- FOREIGN KEY (recipeId) REFERENCES recipes(recipeId),
- FOREIGN KEY (ingredientID) REFERENCES ingredients(ingredientID)
- );
-  DROP TABLE recipesIngredients;
- 
-DELIMITER $$
-CREATE PROCEDURE addnewUser (IN username VARCHAR(255), IN password VARCHAR(255))
-BEGIN
-    INSERT INTO users (userName, password) VALUES (username, password);
-END $$
-
-
-DELIMITER $$
-CREATE PROCEDURE loginCreds (IN username VARCHAR(255), IN password VARCHAR(255))
-BEGIN
-    SELECT * FROM users WHERE userName = username AND password = password;
-END$$ 
